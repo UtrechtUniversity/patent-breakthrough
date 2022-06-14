d16390f231fc6e3ac2bf813e4b79b478d77db3ef
Raoul Schram <r.d.schram@uu.nl>
2022-06-14 11:49:44 +0200

    Fix pylint errors

---
+++ docembedder/__init__.py
@@ -0,0 +1,1 @@
+"""Document embedding package."""
--- docembedder/base.py
+++ docembedder/base.py
@@ -1,4 +1,6 @@
-from abc import ABC, abstractmethod, abstractproperty
+"""Base class for document embeddings."""
+
+from abc import ABC, abstractmethod
 from typing import Iterable, Union
 
 import numpy as np
@@ -6,6 +8,7 @@ import numpy.typing as npt
 
 
 class BaseDocEmbedder(ABC):
+    """Base class for creating document embeddings."""
     @abstractmethod
     def fit(self, documents: Iterable[str]) -> None:
         """Train the model on documents."""
@@ -14,6 +17,7 @@ class BaseDocEmbedder(ABC):
     def predict(self, document: Union[str, Iterable[str]]) -> npt.NDArray[np.float_]:
         """Get the embedding for a document."""
 
-    @abstractproperty
+    @property
+    @abstractmethod
     def embedding_size(self) -> int:
         """Vector size of the embedding."""
