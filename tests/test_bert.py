24098c7ee27ed64bfcb3c2812856e1f8ac4f8032
Shiva Nadi <s.nadi@uu.nl>
2022-08-23 12:51:14 +0200

    Initial commit

---
+++ tests/test_bert.py
@@ -0,0 +1,25 @@
+""" pytest file for bert.py"""
+import dill as pickle
+
+from docembedder.bert import BERTEmbedder
+
+
+def test_bert():
+    """ Function to test bert.py functionality
+    """
+    documents = [
+        "This is a very interesting sentence",
+        "And here is another one"
+    ]
+
+    embedder = BERTEmbedder()
+    embedder.model_path = "../models/test_patent_embeddings.model"
+    embedder.fit(documents)
+
+    try:
+        with open(embedder.model_path, 'rb') as file:
+            embeddings = pickle.load(file)
+            print(embeddings)
+            assert embeddings.shape == (2, 768)
+    except FileNotFoundError:
+        print(f"file {embedder.model_path} does not exist")
