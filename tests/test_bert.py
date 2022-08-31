c753d93b8aedc25fb0e11710fb0b421b03d9bf09
Shiva Nadi <s.nadi@uu.nl>
2022-08-31 13:17:18 +0200

    Remove try/except

--- tests/test_bert.py
+++ tests/test_bert.py
@@ -15,10 +15,7 @@ def test_bert():
     embedder = BERTEmbedder(model_path="./models/test_document_embeddings.dill")
     embedder.fit(documents)
 
-    try:
-        with open(embedder.model_path, 'rb') as file:
-            embeddings = pickle.load(file)
-            print(embeddings)
-            assert embeddings.shape == (2, 768)
-    except FileNotFoundError:
-        print(f"file {embedder.model_path} does not exist")
+    with open(embedder.model_path, 'rb') as file:
+        embeddings = pickle.load(file)
+        print(embeddings)
+        assert embeddings.shape == (2, 768)
