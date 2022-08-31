20857ba125703adea28c2c09e3c1ef6e1e4b76b3
Shiva Nadi <s.nadi@uu.nl>
2022-08-31 11:09:59 +0200

    Detect FileNotFound issue in init

--- docembedder/preparation.py
+++ docembedder/preparation.py
@@ -1,10 +1,9 @@
 """
 Script for adding Patent-data into the memory
 """
-
-from pathlib import Path
 import glob
 import json
+
 import pandas as pd
 
 
@@ -15,9 +14,9 @@ class DOCPreparation:
     ---------
     path: path to the *.jsol files
     """
-    def __init__(self, path=Path(__file__).parent / "../data"):
-        self.path_to_patent_files = path
-        self.all_files = glob.glob(str(self.path_to_patent_files) + "/*.jsonl")
+    def __init__(self):
+        self.all_files = glob.glob("./data" + "/*.jsonl")
+        print("File does not appear to exist.") if not self.all_files else 0
         self._li = []
 
     def read_patents(self):
@@ -25,17 +24,12 @@ class DOCPreparation:
         """
         if self.all_files:
             for filename in self.all_files:
-                try:
-                    with open(filename, encoding="utf-8") as file:
-                        sub_patent_df = pd.DataFrame(json.loads(line) for line in file)
-                        self._li.append(sub_patent_df)
-                except IOError:
-                    print(filename, "File does not appear to exist.")
+                with open(filename, encoding="utf-8") as file:
+                    sub_patent_df = pd.DataFrame(json.loads(line) for line in file)
+                    self._li.append(sub_patent_df)
 
             patent_df = pd.concat(self._li, axis=0, ignore_index=True)
             return patent_df["contents"].to_frame(name="contents")
-        print("File does not appear to exist.")
-        return None
 
     def preprocess_patent(self, patent_data):
         """method for preprocessing patents data"""
