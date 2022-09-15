7a3f33a5347ffefe14f0bc4d73e14b48de283ff7
maartenschermer <m.d.schermer@uu.nl>
2022-09-15 15:38:55 +0200

    structural changes

--- preprocessor/preprocessor.py
+++ preprocessor/preprocessor.py
@@ -1,40 +1,72 @@
-import argparse, glob, logging, json, re, os
+"""
+Preprocessor for patent texts
+"""
+import argparse
+import glob
+import logging
+import json
+import re
+import os
+
 
 class Preprocessor:
+    """
+    Preprocessor class
+    """
 
-    ARGS = None
-    file_list = []
-    current_file = None
-    current_document = None
-    new_file = None
-    doc_property_content = 'contents'
-    doc_property_patent_number = 'patent'
-    valid_single_letter_words = ['a','i']
-    remove_non_alpha = False
-    keep_caps = False
-    keep_start_section = False
+    def __init__(self):
+        self.logger = None
+        self.keep_empty_patents = False
+        self.keep_missing_years = False
+        self.keep_caps = False
+        self.keep_start_section = False
+        self.remove_non_alpha = False
+        self.output_dir = None
+        self.file_list = []
 
-    def __init__(self,log_level=logging.INFO,log_file=None):
+    def initialize(
+            self,
+            log_level: int = logging.INFO,
+            log_file: str = None,
+            log_format: str = '%(asctime)s [%(levelname)s] %(message)s',
+            keep_empty_patents: bool = False,
+            keep_missing_years: bool = False,
+            keep_caps: bool = False,
+            keep_start_section: bool = False,
+            remove_non_alpha: bool = False,
+            input_dir: str = None,
+            output_dir: str = None,
+            ):
+        """Initializes all vars"""
+
         self.logger = logging.getLogger('preprocessor')
         self.logger.setLevel(log_level)
-        ch = logging.StreamHandler()
-        ch.setLevel(log_level)
-        ch.setFormatter(logging.Formatter('%(asctime)s [%(levelname)s] %(message)s'))
-        self.logger.addHandler(ch)
+        slog = logging.StreamHandler()
+        slog.setLevel(log_level)
+        slog.setFormatter(logging.Formatter(log_format))
+        self.logger.addHandler(slog)
+
         if log_file:
-            self.set_log_file(log_file,log_level)
+            flog = logging.FileHandler(log_file)
+            flog.setLevel(log_level)
+            flog.setFormatter(logging.Formatter(log_format))
+            self.logger.addHandler(flog)
 
-    def set_log_file(self,log_file,log_level=logging.INFO):
-        fh = logging.FileHandler(log_file)
-        fh.setLevel(log_level)
-        fh.setFormatter(logging.Formatter('%(asctime)s [%(levelname)s] %(message)s'))
-        self.logger.addHandler(fh)
+        self.keep_empty_patents = keep_empty_patents
+        self.keep_missing_years = keep_missing_years
+        self.keep_caps = keep_caps
+        self.keep_start_section = keep_start_section
+        self.remove_non_alpha = remove_non_alpha
 
-    def parse_arguments(self):
-        """Parses command line parameters"""
-        parser = argparse.ArgumentParser(
-            description="Cleans patents"
-        )
+        # self.input_dir = input_dir
+        self.output_dir = output_dir
+        os.makedirs(self.output_dir, exist_ok=True)
+        self.file_list = self.get_file_list(input_dir)
+
+    def from_arguments(self):
+        """Parses command line parameters and initiates class"""
+        parser = argparse.ArgumentParser(description="Cleans patents")
+
         # Path to the folder that contains the text files
         parser.add_argument(
             "--input",
@@ -50,214 +82,246 @@ class Preprocessor:
             default=None,
             help='output directory string e.g. "../cleaned/"'
         )
-
         parser.add_argument('--remove_non_alpha', action='store_true')
         parser.add_argument('--keep_caps', action='store_true')
         parser.add_argument('--keep_start_section', action='store_true')
-        parser.add_argument('--log_file',type=str)
+        parser.add_argument('--keep_empty_patents', action='store_true')
+        parser.add_argument('--keep_missing_years', action='store_true')
+        parser.add_argument('--log_file', type=str)
+        args = vars(parser.parse_args())
 
-        self.ARGS = vars(parser.parse_args())
+        self.initialize(
+            log_file=args['log_file'],
+            keep_empty_patents=args['keep_empty_patents'],
+            keep_missing_years=args['keep_missing_years'],
+            keep_caps=args['keep_caps'],
+            keep_start_section=args['keep_start_section'],
+            remove_non_alpha=args['remove_non_alpha'],
+            input_dir=args['input'],
+            output_dir=args['output'],
+        )
 
-        self.set_remove_non_alpha(self.ARGS['remove_non_alpha'])
-        self.set_keep_caps(self.ARGS['keep_caps'])
-        self.set_keep_start_section(self.ARGS['keep_start_section'])
+    @staticmethod
+    def get_file_list(input_dir) -> list[str]:
+        """Reads files from input directory"""
+        return glob.glob(input_dir)
 
-        os.makedirs(self.ARGS['output'], exist_ok=True)
-
-        if self.ARGS['log_file']:
-            self.set_log_file(self.ARGS['log_file'])
-
-    def get_args(self):
-        return self.ARGS
-
-    def set_doc_property_content(self,doc_property_content):
-        self.doc_property_content = doc_property_content
-
-    def set_doc_property_patent_number(self,doc_property_patent_number):
-        self.doc_property_patent_number = doc_property_patent_number
-
-    def set_remove_non_alpha(self,remove_non_alpha):
-        self.remove_non_alpha = remove_non_alpha
-
-    def set_keep_caps(self,keep_caps):
-        self.keep_caps = keep_caps
-
-    def set_keep_start_section(self,keep_start_section):
-        self.keep_start_section = keep_start_section
-
-    def read_folder(self):
-        self.file_list = glob.glob(self.ARGS['input'])
-
-    def print_settings(self):
-        self.logger.info(f"input: {self.ARGS['input']}")
-        self.logger.info(f"output: {self.ARGS['output']}")
-        self.logger.info(f"remove_non_alpha: {self.remove_non_alpha}")
-        self.logger.info(f"keep_caps: {self.keep_caps}")
-
-    def process_files(self):
+    def preprocess_files(self):
+        """Iterates all input JSONL-files and calls preprocessing for each"""
         for file in self.file_list:
-            self.logger.info(f'processing {file}')
-            self.current_file = file
-            t = os.path.splitext(os.path.basename(self.current_file))
-            self.new_file = os.path.join(self.ARGS['output'],t[0]+'_cleaned'+t[1])
-            self.process_file()
+            processed, empty, no_year = self.preprocess_file(file)
+            self.logger.info(f'processed {file} ({processed:,} documents, ' +
+                             f'skipped {empty:,} empty, skipped {no_year:,} ' +
+                             'w/o year)')
+        self.logger.info("done")
 
-    def process_file(self):
-        f = open(self.current_file, "r")
-        self.f_new = open(self.new_file, "w")
+    @staticmethod
+    def yield_document(file: str):
+        """Generator yielding single JSON-doc from input file"""
+        with open(file) as handle:
+            line = handle.readline()
+            while line:
+                yield json.loads(line)
+                line = handle.readline()
 
-        while True:
-            line = f.readline()
-            if not line:
-                break
-            self.current_document = json.loads(line)
-            self.remove_unprintable()
-            self.remove_start_section()
-            self.clean_document()
-            self.remove_remains()
-            self.write_document()
+    def preprocess_file(self, file: str):
+        """Iterates individual JSON-docs in JSONL-file and calls preprocsseing
+        for each"""
+        parts = os.path.splitext(os.path.basename(file))
+        new_file = os.path.join(self.output_dir, parts[0]+'_cleaned'+parts[1])
+        processed = 0
+        skipped_empty = 0
+        skipped_no_year = 0
+        with open(new_file, "w") as new_file:
+            gen = self.yield_document(file)
+            while True:
+                patent = next(gen, False)
+                if not patent:
+                    break
 
-        f.close()
-        self.f_new.close()
+                if patent['year'] == 0 or patent['year'] is None:
+                    self.logger.warning(f'Patent #{patent["patent"]} has ' +
+                                        'no year')
+                    if not self.keep_missing_years:
+                        skipped_no_year += 1
+                        continue
 
-    def remove_unprintable(self):
-        content = self.current_document[self.doc_property_content]
-        # remove unprintable characters
-        cleaned = re.sub(r'[\u0080-\uffff]', "", content)
-        self.current_document[self.doc_property_content] = cleaned
+                body = patent['contents']
 
-    def remove_remains(self):
-        content = self.current_document[self.doc_property_content]
-        # remove non-alphanumeric substrings in words
+                if len(body) == 0:
+                    self.logger.warning(f'Patent #{patent["patent"]} has ' +
+                                        'no content')
+                    if not self.keep_empty_patents:
+                        skipped_empty += 1
+                        continue
+
+                body = self.remove_unprintable(body)
+                body = self.remove_start_section(body)
+                body = self.clean_document(body)
+                body = self.remove_remains(body)
+
+                patent['contents'] = body
+                self.write_document(new_file, patent)
+                processed += 1
+
+        return processed, skipped_empty, skipped_no_year
+
+    @staticmethod
+    def remove_unprintable(content: str) -> str:
+        """Removes unprintable characters"""
+        return re.sub(r'[\u0080-\uffff]', "", content)
+
+    @staticmethod
+    def remove_remains(content: str) -> str:
+        """Replaces multiple spaces with single ones"""
         cleaned = re.sub(r'(\B[^\w\s]+\B)', '', content)
-        # replace multiple spaces with single ones
-        cleaned = re.sub(r'[ ]+',' ',cleaned)
-        self.current_document[self.doc_property_content] = cleaned.strip()
+        cleaned = re.sub(r'[ ]+', ' ', cleaned)
+        return cleaned.strip()
 
-    def clean_document(self):
-        content = self.current_document[self.doc_property_content]
-
-        WORD_PATTERN = re.compile(r'\w+')
-        content_cleaned = WORD_PATTERN.sub(
+    def clean_document(self, content: str) -> str:
+        """Go through all words in document and call word processor."""
+        word_pattern = re.compile(r'\w+')
+        content_cleaned = word_pattern.sub(
             lambda word: self.process_word(word.group(0)),
             content
         )
+        return content_cleaned
 
-        self.current_document[self.doc_property_content] = content_cleaned
-
-    def process_word(self,word):
+    def process_word(self, word: str) -> str:
+        """Processes individual words"""
         lower_word = word.lower()
+        contains_only_ident_chars = re.compile(r'\b([a-z])\1{1,}\b')
+        contains_multiple_ident_chars = \
+            re.compile(r'([a-z])\1{2,}')
+        valid_single_letter_words = ['a', 'i']
 
-        # regex for consecutive identical characters
-        # CONTAINS_IDENT_CHARS = re.compile(r'(\w)\1{2,}')
-        CONTAINS_ONLY_IDENT_CHARS = re.compile(r'\b(([a-z])\2+)\b')
-        CONTAINS_MULTIPLE_IDENT_CHARS = re.compile(r'\b([a-z]+)(([a-z])\3{3,})([a-z]+)\b')
-
-        if (len(lower_word) < 2 and not lower_word in self.valid_single_letter_words) \
-            or (self.remove_non_alpha and not(lower_word.isalpha())) \
-            or bool(CONTAINS_ONLY_IDENT_CHARS.search(lower_word)) \
-            or bool(CONTAINS_MULTIPLE_IDENT_CHARS.search(lower_word)):
+        if (len(lower_word) < 2 and
+                lower_word not in valid_single_letter_words):
+            # print(f"x [single]: {word}")
             return ''
 
+        if (self.remove_non_alpha and not lower_word.isalpha()):
+            # print(f"x [non-alpha]: {word}")
+            return ''
+
+        if bool(contains_only_ident_chars.search(lower_word)):
+            # print(f"x [ident]: {word}")
+            return ''
+
+        if bool(contains_multiple_ident_chars.search(lower_word)):
+            # print(f"x [multi]: {word}")
+            return ''
+
+        # print(f"v: {word}")
         return word if self.keep_caps else lower_word
 
-    def write_document(self):
-        self.f_new.write(json.dumps(self.current_document) + "\n")
+    @staticmethod
+    def write_document(file_handle, patent: dict):
+        """Writes processed docs"""
+        file_handle.write(json.dumps(patent) + "\n")
 
-    def count_upper_case_letters(self,str_obj):
+    @staticmethod
+    def count_upper_case_letters(str_obj: str) -> int:
+        """Counts the number of uppercase letters in a string"""
         count = 0
         for elem in str_obj:
             if elem.isupper():
                 count += 1
         return count
 
-    def chunker(self, seq, size):
-        return (seq[pos:pos + size] for pos in range(0, len(seq), size))
+    @staticmethod
+    def chunker(seq, size) -> list[str]:
+        """Returns a chunk of a list of strings"""
+        return list(seq[pos:pos + size] for pos in range(0, len(seq), size))
 
-    def remove_start_section(self):
-        if self.keep_start_section:
-            return
-
-        # get content body
-        content = self.current_document[self.doc_property_content]
-
-        if len(content)==0:
-            self.logger.warning(f"Patent #{self.current_document[self.doc_property_patent_number]} has no content")
-            return
-
-        # split into words & clean
+    def split_and_clean(self, content: str) -> str:
+        """Split content into words and clean them"""
         raw_words = content.split()
-        words = [x for x in raw_words if len(self.process_word(x)) > 0 ]
+        words = [x for x in raw_words if len(self.process_word(x)) > 0]
+        return words
 
-        if len(words)==0:
-            self.logger.warning(f"Patent #{self.current_document[self.doc_property_patent_number]} has no words")
-            return
+    def remove_start_section(self, content: str,
+                             frac_threshold: float = 0.60) -> str:
+        """Removes the start section of a patent, identified by a large
+        fraction of capital letters in the text"""
+        if self.keep_start_section or len(content) == 0:
+            return content
 
-        frac_threshold = 0.60
+        words = self.split_and_clean(content)
 
-        # go through the first 1000 words, and break when the overall percentage of characters that are CAPS falls
-        # below the threshold
+        if len(words) == 0:
+            return content
+
+        first_words = self.get_first_words(words=words,
+                                           frac_threshold=frac_threshold)
+        start_section = self.get_start_section(first_words=first_words,
+                                               frac_threshold=frac_threshold)
+
+        return " ".join(words)[len(start_section):].strip()
+
+    def get_first_words(self, words: list[str],
+                        frac_threshold: float) -> list[str]:
+        """Go through the first 1000 words, and break when the overall
+        percentage of characters that are CAPS falls below the threshold"""
         tot_cap = 0
         tot_len = 0
+        idx = 0
         for idx, word in enumerate(words[0:1000]):
             tot_cap += self.count_upper_case_letters(word)
             tot_len += len(word)
             if (idx > 10 and (tot_cap/tot_len) < frac_threshold):
                 break
 
-        first_words = words[0:idx]
-        # print(first_words)
+        return words[0:idx]
 
-        # go through the remaining words in chunks of three words at a time, and again calculate the percentage
-        # of CAPS-characters per chunk, and stop after it falls below the threshold. the first three chunks are
-        # always included (some patents start with a few lowercase words)
-        chunk_size = 3
-        chunks = list(self.chunker(first_words, chunk_size))
+    def get_start_section(self, first_words: list, frac_threshold: float):
+        """Go through the remaining words in chunks of three words at a time,
+        and again calculate the percentage of CAPS-characters per chunk, and
+        stop after it falls below the threshold. the first three chunks are
+        always included (some patents start with a few lowercase words)"""
+        chunks = self.chunker(first_words, 3)
 
-        if len(chunks)==0:
+        if len(chunks) == 0:
+            return ""
 
-            start_section = ""
+        idx = 0
+        chunk = []
+        for idx, chunk in enumerate(chunks):
+            c_upper = sum(map(self.count_upper_case_letters, chunk))
+            c_all = sum(map(len, chunk))
+            frac = (c_upper / c_all)
+            if idx > 10 and frac < frac_threshold:
+                break
 
-        else:
+        # join the chunks
+        first_part = []
+        for item in list(chunks)[:idx]:
+            first_part += item
 
-            for idx, chunk in enumerate(chunks):
-                c_upper = sum(map(self.count_upper_case_letters, chunk))
-                c_all = sum(map(len, chunk))
-                frac = (c_upper / c_all)
-                # print(frac,chunk)
-                if idx > 10 and  frac < frac_threshold:
-                    break
+        # examine the last chunk, which fell below the threshold, but might
+        # contain some uppercase words at the start
+        rest = []
+        for word in chunk:
+            if self.count_upper_case_letters(word) / len(word) >= \
+                                                     frac_threshold:
+                rest.append(word)
+            else:
+                break
 
-            # join the chunks
-            first_part = []
-            for item in list(chunks)[:idx]:
-                first_part += item
+        start_section = first_part + rest
+        for idx, item in enumerate(reversed(start_section)):
+            if (self.count_upper_case_letters(item) / len(item)) > frac:
+                break
 
-            # examine the last chunk, which fell below the threshold, but might contain some uppercase words at the start
-            rest = []
-            for word in chunk:
-                if (self.count_upper_case_letters(word) / len(word) >= frac_threshold):
-                    rest.append(word)
-                else:
-                    break
-
-            start_section = first_part + rest
-            for idx,item in enumerate(reversed(start_section)):
-                if (self.count_upper_case_letters(item) / len(item)) > frac:
-                    break
-
-            if idx > 0:
-                start_section = start_section[0:len(start_section)-idx]
+        if idx > 0:
+            start_section = start_section[0:len(start_section)-idx]
 
         # reassemble start section, and chop it from the original body
         start_section = " ".join(start_section)
-        self.current_document[self.doc_property_content] = " ".join(words)[len(start_section):].strip()
-        self.logger.info(f"Patent #{self.current_document[self.doc_property_patent_number]} has {len(words)} words")
+        return start_section
+
 
 if __name__ == '__main__':
-
-    pp = Preprocessor()
-    pp.parse_arguments()
-    pp.print_settings()
-    pp.read_folder()
-    pp.process_files()
+    p = Preprocessor()
+    p.from_arguments()
+    p.preprocess_files()
