b6e3957f39cbeb37a4450b813e95ad1bfb4bb383
Shiva Nadi <s.nadi@uu.nl>
2022-10-07 12:57:13 +0200

    Add documentation

--- docembedder/doc2vec.py
+++ docembedder/doc2vec.py
@@ -31,14 +31,26 @@ logging.basicConfig(format="%(levelname)s - %(asctime)s: %(message)s",
 
 
 class D2VEmbedder(BaseDocEmbedder):
-    """ class for calculating Document vectors
+    """ Doc2Vec
+    class for representing each Document as a Vector using Genism Doc2Vec model
+
+    useful links:
+    https://radimrehurek.com/gensim/models/doc2vec.html
+    https://radimrehurek.com/gensim/auto_examples/tutorials/run_doc2vec_lee.html#sphx-glr-auto-examples-tutorials-run-doc2vec-lee-py
+
+
+    Arguments
+    ----------
+    vector_size: Dimensionality of the feature vectors
+    min_count: Ignores all words with total frequency lower than this
+    epoch: Number of iterations (epochs) over the corpus
+    workers: Number of worker threads to train the model (faster training with multicore machines)
     """
-    def __init__(self, vector_size=100, min_count=2, epoch=10, workers=4):
+    def __init__(self, vector_size: int = 100, min_count: int = 2, epoch: int = 10, workers: int = 4):
         self.vector_size = vector_size
         self.min_count = min_count
         self.epoch = epoch
         self.workers = 4
-        self._tagged_data = []
 
         self._d2v_model = gensim.models.doc2vec.Doc2Vec(
             vector_size=vector_size, min_count=min_count, epochs=epoch, workers=workers)
