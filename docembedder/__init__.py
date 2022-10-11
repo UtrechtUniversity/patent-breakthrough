90019ff83b1425c1864b2ff21727e73e662af3e5
Shiva Nadi <44059592+ShNadi@users.noreply.github.com>
2022-10-11 12:52:18 +0200

    Doc2vec (#19)
    * Initial commit
    
    * Fix pylint issues
    
    * Add nltk, gensim
    
    * Change vector size
    
    * import D2VEmbedder
    
    * uncomment ssl
    
    * Add nltk/gensim
    
    * Move taggeddocument/buildvocab to fit
    
    * Add get_vector()
    
    * Add documentation
    
    * Fix pylint issues
    
    * Remome main/ Add embedding_size()
    
    * Fix pylint issues
    
    * Add train()
    
    * Fix pylint
    
    * Remove nltk.download/ssl
    
    * Move train to fit
    
    * Update docembedder/doc2vec.py
    
    Edit documentation
    
    Co-authored-by: qubixes <44498096+qubixes@users.noreply.github.com>
    
    * Update docembedder/doc2vec.py
    
    Edit documentation
    
    Co-authored-by: qubixes <44498096+qubixes@users.noreply.github.com>
    
    * Update docembedder/doc2vec.py
    
    Edit documentation
    
    Co-authored-by: qubixes <44498096+qubixes@users.noreply.github.com>
    
    * Update docembedder/doc2vec.py
    
    Edit documentation
    
    Co-authored-by: qubixes <44498096+qubixes@users.noreply.github.com>
    
    * Add nltk.download(punkt)to __init__
    
    * Remove main
    
    * Use infer_vector instead of dv in transform()
    
    * Use of enumerate instead of len in transform()
    
    * Remove vector variable from transform
    
    * Fix mypy issues
    
    * Remove double import
    
    * Add test for doc2vec
    
    * change vector size
    
    * Import D2VEmbedder
    
    * Add try block in init for solving certificate error while loading punk
    
    * Update __init__.py
    
    Remove line form the end of file
    
    * Update test_doc2vec.py
    
    Remove main
    
    Co-authored-by: qubixes <44498096+qubixes@users.noreply.github.com>

--- docembedder/__init__.py
+++ docembedder/__init__.py
@@ -3,3 +3,4 @@
 from docembedder.tfidf import TfidfEmbedder
 from docembedder.bert import BERTEmbedder
 from docembedder.preprocessor.preprocessor import Preprocessor
+from docembedder.doc2vec import D2VEmbedder
---
+++ docembedder/doc2vec.py
@@ -0,0 +1,78 @@
+""" Gensim Doc2vec class."""
+
+import logging
+from typing import Iterable, Union, List
+
+import ssl
+
+import numpy as np
+from nltk.tokenize import word_tokenize
+from gensim.models.doc2vec import TaggedDocument
+import gensim
+import nltk
+
+from docembedder.base import BaseDocEmbedder
+
+
+class D2VEmbedder(BaseDocEmbedder):
+    """ Doc2Vec
+    class for representing each Document as a Vector using Genism Doc2Vec model
+
+    useful links:
+    https://radimrehurek.com/gensim/models/doc2vec.html
+    https://radimrehurek.com/gensim/auto_examples/tutorials/run_doc2vec_lee.html#sphx-glr-auto-examples-tutorials-run-doc2vec-lee-py
+
+
+    Arguments
+    ---------
+    vector_size: Dimensionality of the feature vectors
+    min_count: Ignores all words with total frequency lower than this
+    epoch: Number of iterations (epochs) over the corpus
+    workers: Number of worker threads to train the model (faster training with multicore machines)
+    """
+    def __init__(self,
+                 vector_size: int = 100,
+                 min_count: int = 2,
+                 epoch: int = 10,
+                 workers: int = 4):
+        self.vector_size = vector_size
+        self.min_count = min_count
+        self.epoch = epoch
+        self.workers = 4
+        self._tagged_data: List = []
+
+        # Solving CERTIFICATE_VERIFY_FAILED while loading punk
+        try:
+            _create_unverified_https_context = ssl._create_unverified_context  # pylint: disable=W0212
+        except AttributeError:
+            pass
+        else:
+            ssl._create_default_https_context = _create_unverified_https_context  # pylint: disable=W0212
+
+        logging.basicConfig(format="%(levelname)s - %(asctime)s: %(message)s",
+                            datefmt='%H:%M:%S', level=logging.INFO)
+        nltk.download('punkt')
+
+        self._d2v_model = gensim.models.doc2vec.Doc2Vec(
+            vector_size=vector_size, min_count=min_count, epochs=epoch, workers=workers)
+
+    def fit(self, documents: Iterable[str]) -> None:
+        logging.info("Building Doc2Vec vocabulary:")
+        self._tagged_data = [
+            TaggedDocument(words=word_tokenize(_d.lower()),
+                           tags=[str(i)]) for i, _d in enumerate(documents)]
+        self._d2v_model.build_vocab(self._tagged_data)
+        self._d2v_model.train(
+            self._tagged_data, total_examples=self._d2v_model.corpus_count, epochs=self.epoch)
+
+    def transform(self, documents: Union[str, Iterable[str]]) -> List[np.float_]:
+        logging.info("Extracting Document vectors:")
+        vectors = [
+            self._d2v_model.infer_vector(
+                doc_words=word_tokenize(_d.lower())) for i, _d in enumerate(documents)]
+
+        return vectors
+
+    @property
+    def embedding_size(self) -> int:
+        return self.vector_size
