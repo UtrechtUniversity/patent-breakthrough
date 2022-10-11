8e68784ab60ec810cf5e8752d48533483d7c094a
Shiva Nadi <44059592+ShNadi@users.noreply.github.com>
2022-10-11 11:43:45 +0200

    Update __init__.py
    Remove line form the end of file

--- docembedder/__init__.py
+++ docembedder/__init__.py
@@ -4,4 +4,3 @@ from docembedder.tfidf import TfidfEmbedder
 from docembedder.bert import BERTEmbedder
 from docembedder.preprocessor.preprocessor import Preprocessor
 from docembedder.doc2vec import D2VEmbedder
-
