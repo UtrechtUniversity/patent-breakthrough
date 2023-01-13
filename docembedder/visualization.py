92015f6a0e668326e2e0777909d43d43d311768c
Raoul Schram <r.d.schram@uu.nl>
2023-01-13 12:53:26 +0100

    Bigger pictures

--- docembedder/visualization.py
+++ docembedder/visualization.py
@@ -5,6 +5,7 @@ from matplotlib import pyplot as plt
 
 def plot_cpc_correlations(correlations):
     """Plot correlation of embeddings with CPC classifications."""
+    plt.figure(dpi=100)
     for model_name, cor_data in correlations.items():
         plt.plot(cor_data["year"], cor_data["correlations"], label=model_name)
     plt.legend()
