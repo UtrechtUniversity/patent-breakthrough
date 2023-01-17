3c4e937aaae0c16ea5816e128e142386c9fb4e81
Raoul Schram <r.d.schram@uu.nl>
2023-01-17 10:13:43 +0100

    Add versioning to docembedder

--- docembedder/__init__.py
+++ docembedder/__init__.py
@@ -5,3 +5,6 @@ from docembedder.analysis import DOCSimilarity
 from docembedder.preprocessor.parser import read_xz
 from docembedder.datamodel import DataModel
 from docembedder.models.base import AllEmbedType
+
+from . import _version
+__version__ = _version.get_versions()['version']
--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 # based on https://github.com/pypa/sampleproject - MIT License
-
 from setuptools import setup, find_packages
+
+import versioneer
 
 setup(
     name='docembedder',
     version='0.0.1',
     author='UU Research Engineering Team',
     description='Package for creating document embeddings',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     long_description='Package for creating document embeddings',
     packages=find_packages(exclude=['data', 'docs', 'tests', 'examples']),
     python_requires='~=3.6',
