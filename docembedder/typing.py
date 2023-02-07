9c5828e0630c99ef71f9c4f437f7e73b401734b5
qubixes <44498096+qubixes@users.noreply.github.com>
2023-02-07 16:57:05 +0100

    Support io.BytesIO format for running analysis in memory only

--- docembedder/typing.py
+++ docembedder/typing.py
@@ -1,6 +1,7 @@
 """Type Aliases for static typing."""
 
 from __future__ import annotations
+import io
 from pathlib import Path
 from typing import Union, Sequence
 
@@ -13,3 +14,4 @@ import numpy as np
 AllEmbedType: TypeAlias = Union[scipy.sparse.spmatrix, npt.NDArray[np.float_]]
 PathType: TypeAlias = Union[Path, str]
 IntSequence: TypeAlias = Union[Sequence[int], npt.NDArray[np.int_]]
+FileType: TypeAlias = Union[PathType, io.BytesIO]
