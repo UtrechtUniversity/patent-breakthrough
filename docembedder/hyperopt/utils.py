62d5510144b2a5c61feb8697d0b197179e3d0541
maartenschermer <m.d.schermer@uu.nl>
2023-02-08 10:48:27 +0100

    linting

--- docembedder/hyperopt/utils.py
+++ docembedder/hyperopt/utils.py
@@ -1,4 +1,4 @@
-from typing import Optional, Callable, Any, Dict
+from typing import Optional, Any, Dict
 from pathlib import Path
 import json
 from hashlib import md5
@@ -10,16 +10,19 @@ from docembedder.models import TfidfEmbedder, D2VEmbedder, CountVecEmbedder, BPe
 from docembedder.utils import SimulationSpecification
 from docembedder import DataModel
 from docembedder.analysis2 import DocAnalysis
+from docembedder.preprocessor.preprocessor import Preprocessor
+from docembedder.models.base import BaseDocEmbedder
 from hyperopt import STATUS_OK, fmin, tpe
 
-class ModelHyperopt(): # pylint: disable=too-many-instance-attributes
+
+class ModelHyperopt():  # pylint: disable=too-many-instance-attributes
 
     def __init__(self,  # pylint: disable=too-many-arguments
                  year_start: int,
                  year_end: int,
                  window_size: int,
                  n_jobs: int,
-                 preprocessors: Optional[list[Callable]],
+                 preprocessors: Optional[Dict[str, Preprocessor]],
                  cpc_fp: Path,
                  patent_dir: Path
                  ) -> None:
@@ -34,39 +37,39 @@ class ModelHyperopt(): # pylint: disable=too-many-instance-attributes
 
     def optimize_tfidf(self, max_evals: int = 10) -> None:
         self.best['tfidf'] = fmin(self._tfidf_objective_func,
-            space=TfidfEmbedder.hyper_space(),
-            algo=tpe.suggest,
-            max_evals=max_evals)
+                                  space=TfidfEmbedder.hyper_space(),
+                                  algo=tpe.suggest,
+                                  max_evals=max_evals)
 
     def optimize_d2v(self, max_evals: int = 10) -> None:
         self.best['d2v'] = fmin(self._d2v_objective_func,
-            space=D2VEmbedder.hyper_space(),
-            algo=tpe.suggest,
-            max_evals=max_evals)
+                                space=D2VEmbedder.hyper_space(),
+                                algo=tpe.suggest,
+                                max_evals=max_evals)
 
     def optimize_countvec(self, max_evals: int = 10) -> None:
         self.best['countvec'] = fmin(self._countvec_objective_func,
-            space=CountVecEmbedder.hyper_space(),
-            algo=tpe.suggest,
-            max_evals=max_evals)
+                                     space=CountVecEmbedder.hyper_space(),
+                                     algo=tpe.suggest,
+                                     max_evals=max_evals)
 
     def optimize_bpemp(self, max_evals: int = 10) -> None:
         self.best['bpemp'] = fmin(self._bpemp_objective_func,
-            space=BPembEmbedder.hyper_space(),
-            algo=tpe.suggest,
-            max_evals=max_evals)
+                                  space=BPembEmbedder.hyper_space(),
+                                  algo=tpe.suggest,
+                                  max_evals=max_evals)
 
     def optimize_bert(self, max_evals: int = 10) -> None:
         self.best['bert'] = fmin(self._bert_objective_func,
-            space=BERTEmbedder.hyper_space(),
-            algo=tpe.suggest,
-            max_evals=max_evals)
+                                 space=BERTEmbedder.hyper_space(),
+                                 algo=tpe.suggest,
+                                 max_evals=max_evals)
 
     @classmethod
     def hash_params(cls, prefix: str, params: Dict[str, Any]) -> str:
         return prefix + md5(json.dumps(params).encode('utf-8')).hexdigest()
 
-    def _general_objective_func(self, label: str, model: object) -> Dict[str, Any]:
+    def _general_objective_func(self, label: str, model: BaseDocEmbedder) -> Dict[str, Any]:
         models = {label: model}
         sim_spec = SimulationSpecification(year_start=self.year_start,
                                            year_end=self.year_end, window_size=self.window_size)
@@ -77,7 +80,7 @@ class ModelHyperopt(): # pylint: disable=too-many-instance-attributes
                    patent_dir=self.patent_dir, output_fp=output_fp, cpc_fp=self.cpc_fp,
                    n_jobs=self.n_jobs)
 
-        pp_prefix = "default-" # if preprocessors is None else "..."
+        pp_prefix = "default-"
 
         with DataModel(output_fp, read_only=False) as data:
             analysis = DocAnalysis(data)
@@ -88,7 +91,7 @@ class ModelHyperopt(): # pylint: disable=too-many-instance-attributes
 
     def _tfidf_objective_func(self, params: Dict[str, Any]) -> Dict[str, Any]:
         return self._general_objective_func(
-            label=self.hash_params(prefix="tfidf-",params=params),
+            label=self.hash_params(prefix="tfidf-", params=params),
             model=TfidfEmbedder(
                 max_df=params["max_df"],
                 min_df=params["min_df"],
@@ -101,7 +104,7 @@ class ModelHyperopt(): # pylint: disable=too-many-instance-attributes
 
     def _d2v_objective_func(self, params: Dict[str, Any]) -> Dict[str, Any]:
         return self._general_objective_func(
-            label=self.hash_params(prefix="d2v-",params=params),
+            label=self.hash_params(prefix="d2v-", params=params),
             model=D2VEmbedder(
                 vector_size=params["vector_size"],
                 min_count=params["min_count"],
@@ -110,9 +113,9 @@ class ModelHyperopt(): # pylint: disable=too-many-instance-attributes
 
     def _countvec_objective_func(self, params: Dict[str, Any]) -> Dict[str, Any]:
         return self._general_objective_func(
-            label=self.hash_params(prefix="countvec-",params=params),
+            label=self.hash_params(prefix="countvec-", params=params),
             model=CountVecEmbedder(method=params["method"]
-        ))
+                                   ))
 
     def _bpemp_objective_func(self, params: Dict[str, Any]) -> Dict[str, Any]:
         return self._general_objective_func(
@@ -126,4 +129,4 @@ class ModelHyperopt(): # pylint: disable=too-many-instance-attributes
         return self._general_objective_func(
             label=self.hash_params(prefix="bert-", params=params),
             model=BERTEmbedder(pretrained_model=params["pretrained_model"]
-        ))
+                               ))
