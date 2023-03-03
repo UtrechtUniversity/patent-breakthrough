6e9fb6c90d622407b0cd27f7a38ca07aab9cf532
Raoul Schram <r.d.schram@uu.nl>
2023-03-03 18:29:24 +0100

    Update hyperopt tfidf-results

--- docembedder/hyperopt/utils.py
+++ docembedder/hyperopt/utils.py
@@ -1,11 +1,15 @@
 """Utils for model parameter optimization using hyperopt."""
 
-from typing import Optional, Any, Dict, Callable, Type
+from typing import Optional, Any, Dict, Callable, Type, Union, Iterable
 from pathlib import Path
 from collections import defaultdict
 import pickle
+import itertools
 
-from hyperopt import STATUS_OK, fmin, tpe, Trials
+import numpy as np
+from hyperopt import STATUS_OK, fmin, tpe, Trials, space_eval
+from tqdm import tqdm
+import pandas as pd
 
 from docembedder.utils import SimulationSpecification
 from docembedder.preprocessor.preprocessor import Preprocessor
@@ -29,71 +33,45 @@ class ModelHyperopt():
     """
     def __init__(self,  # pylint: disable=too-many-arguments
                  sim_spec: SimulationSpecification,
-                 preprocessors: Optional[Dict[str, Preprocessor]],
                  cpc_fp: Path,
                  patent_dir: Path,
-                 trials: Optional[Trials]=None,
+                 preprocessor: Optional[Preprocessor] = None,
+                 trials: Optional[Union[Dict[str, Trials], PathType]]=None,
                  ) -> None:
         self.sim_spec = sim_spec
-        self.preprocessors = preprocessors
+        if preprocessor is None:
+            preprocessor = Preprocessor()
+        self.preprocessor = preprocessor
         self.cpc_fp = cpc_fp
         self.patent_dir = patent_dir
         self.best: Dict = defaultdict(fmin)
+        self.pickle_fp = None
         if trials is None:
             self.trials: Dict = defaultdict(Trials)
+        elif isinstance(trials, (str, Path)):
+            self.pickle_fp = trials
+            if Path(trials).is_file():
+                with open(trials, "rb") as handle:
+                    self.trials = pickle.load(handle)
+            else:
+                self.trials = defaultdict(Trials)
         else:
             self.trials = trials
 
-    def optimize_model(  # pylint: disable=too-many-arguments
+    def optimize(  # pylint: disable=too-many-arguments
             self,
             label: str,
             model: Type[BaseDocEmbedder],
-            preprocessor: Optional[Preprocessor]=None,
             max_evals: int = 10,
-            n_jobs: int = 10,
-            pickle_fp: Optional[PathType] = None) -> None:
+            n_jobs: int = 10) -> None:
         """Hyperopt optimization function"""
 
         if len(self.trials[label]) >= max_evals:
             return
 
-        if preprocessor is None:
-            preprocessor = Preprocessor()
-
-        objective_function = self.get_model_objective_func(label=label, model=model, n_jobs=n_jobs)
+        objective_function = self.get_objective_func(label=label, model=model, n_jobs=n_jobs)
         space = model.hyper_space()
 
-        if pickle_fp is None:
-            self.best[label] = fmin(objective_function,
-                                    space=space,
-                                    algo=tpe.suggest,
-                                    max_evals=max_evals,
-                                    trials=self.trials[label])
-        else:
-            while len(self.trials[label]) < max_evals:
-                new_evals = min(max_evals, len(self.trials[label])+10)
-                self.best[label] = fmin(objective_function,
-                                        space=space,
-                                        algo=tpe.suggest,
-                                        max_evals=new_evals,
-                                        trials=self.trials[label])
-                with open(pickle_fp, "wb") as handle:
-                    pickle.dump(self.trials, handle)
-
-    def optimize_preprocessor(  # pylint: disable=too-many-arguments
-            self,
-            label: str,
-            model: BaseDocEmbedder,
-            preprocessor: Type[Preprocessor],
-            max_evals: int = 10,
-            n_jobs: int = 10,
-            pickle_fp: Optional[PathType] = None) -> None:
-        """Optimize the preprocessor."""
-        if len(self.trials[label]) >= max_evals:
-            return
-
-        space = preprocessor.hyper_space()
-        objective_function = self.get_preprocessor_objective_func(preprocessor, model, n_jobs)
         while len(self.trials[label]) < max_evals:
             new_evals = min(max_evals, len(self.trials[label])+10)
             self.best[label] = fmin(objective_function,
@@ -101,15 +79,15 @@ class ModelHyperopt():
                                     algo=tpe.suggest,
                                     max_evals=new_evals,
                                     trials=self.trials[label])
-            if pickle_fp is not None:
-                with open(pickle_fp, "wb") as handle:
+            if self.pickle_fp is not None:
+                with open(self.pickle_fp, "wb") as handle:
                     pickle.dump(self.trials, handle)
 
-    def get_model_objective_func(self,
-                                 n_jobs: int=10,
-                                 **kwargs) -> Callable[[Dict[str, Any]], Dict[str, Any]]:
+    def get_objective_func(self,
+                           n_jobs: int=10,
+                           **kwargs) -> Callable[[Dict[str, Any]], Dict[str, Any]]:
         """Creates general loss function"""
-        prep = Preprocessor()
+        prep = self.preprocessor
 
         documents, cpc_cor = get_patent_data_multi(self.sim_spec, prep, self.patent_dir,
                                                    self.cpc_fp, n_jobs=n_jobs)
@@ -121,18 +99,108 @@ class ModelHyperopt():
             return {"loss": -correlation, "status": STATUS_OK}
         return objective_func
 
-    def get_preprocessor_objective_func(
+    def dataframe(self, label, model_class):
+        trial_results = defaultdict(list)
+        for trial in self.trials[label]:
+            if "loss" not in trial["result"]:
+                continue
+            new_trial = {key: val[0] for key, val in trial["misc"]["vals"].items()}
+            conv_trial = space_eval(model_class.hyper_space(), new_trial)
+            for key, val in conv_trial.items():
+                trial_results[key].append(val)
+            trial_results["loss"].append(trial["result"]["loss"])
+        return pd.DataFrame(trial_results).sort_values("loss")
+
+    def best_model(self, label, model_class):
+        result_df = self.dataframe(label, model_class)
+        best_prep_param = {
+            key: list(value.values())[0] for key, value in result_df.head(1).to_dict().items()
+            if key != "loss"}
+        return model_class(**best_prep_param)
+
+
+class PreprocessorHyperopt():
+    """
+    ModelHyperopt class
+
+    Objective and optimization functions, generalized for each model type.
+    Class var 'best' contains the best result of each optimizing function.
+    Class var 'trials' contains the results of optimization process. Each element contains:
+    - trials - a list of dictionaries representing everything about the search
+    - results - a list of dictionaries returned by 'objective' during the search
+    - losses() - a list of losses (float for each 'ok' trial)
+    - statuses() - a list of status strings
+
+    """
+    def __init__(self,  # pylint: disable=too-many-arguments
+                 sim_spec: SimulationSpecification,
+                 cpc_fp: Path,
+                 patent_dir: Path,
+                 trials: Optional[Union[Dict, PathType]]=None,
+                 ) -> None:
+        self.sim_spec = sim_spec
+        self.cpc_fp = cpc_fp
+        self.patent_dir = patent_dir
+        self.best: Dict = defaultdict(fmin)
+        self.pickle_fp = None
+        if trials is None:
+            self.trials: Dict = defaultdict(list)
+        elif isinstance(trials, dict):
+            self.trials = trials
+        elif isinstance(trials, (str, Path)):
+            self.pickle_fp = trials
+            if Path(trials).is_file():
+                with open(trials, "rb") as handle:
+                    self.trials = pickle.load(handle)
+            else:
+                self.trials = defaultdict(list)
+        else:
+            raise ValueError(f"Unknown trials type: '{type(trials)}'")
+
+    def optimize(
             self,
-            preprocessor: Type[Preprocessor],
+            label: str,
             model: BaseDocEmbedder,
-            n_jobs: int = 10,
-            ):
-        """Create the objective function for the preprocessor optimization."""
-        def _objective_func(params: Dict[str, Any]) -> Dict[str, Any]:
+            preprocessor: Type[Preprocessor],
+            n_jobs: int = 10) -> None:
+        """Optimize the preprocessor."""
+        space = preprocessor.hyper_space()
+        assert np.all([sub_space.name == "switch" for sub_space in space.values()])
+        if len(space) > 0:
+            all_settings: Iterable[tuple] = itertools.product(*[[True, False]
+                                                                for _ in range(len(space))])
+        else:
+            all_settings = [()]  # type: ignore
+
+        for setting in tqdm(all_settings, total=2**len(space)):
+            params = {key: setting[i] for i, key in enumerate(space)}
+            trial_done = False
+            for trial in self.trials[label]:
+                if params == trial["params"]:
+                    trial_done = True
+            if trial_done:
+                continue
             prep = preprocessor(**params)
             documents, cpc_cor = get_patent_data_multi(
                 self.sim_spec, prep, self.patent_dir, self.cpc_fp, n_jobs,
                 progress_bar=False)
             correlation = run_jobs(model, documents, cpc_cor, n_jobs)
-            return {"loss": -correlation, "status": STATUS_OK}
-        return _objective_func
+            self.trials[label].append({"loss": -correlation, "params": params})
+            if self.pickle_fp is not None:
+                with open(self.pickle_fp, "wb") as handle:
+                    pickle.dump(self.trials, handle)
+
+    def dataframe(self, label):
+        data_dict = defaultdict(list)
+        for trial in self.trials[label]:
+            for key, value in trial["params"].items():
+                data_dict[key].append(value)
+            data_dict["loss"].append(trial["loss"])
+        return pd.DataFrame(data_dict).sort_values("loss")
+
+    def best_preprocessor(self, label, prep_class):
+        result_df = self.dataframe(label)
+        best_prep_param = {
+            key: list(value.values())[0] for key, value in result_df.head(1).to_dict().items()
+            if key != "loss"}
+        return prep_class(**best_prep_param)
